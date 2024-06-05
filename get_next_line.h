b087f86053c19fadaf9c8037e07d607489b4d515
Maxime Han <mhan@made-f0Ar12s10.clusters.42paris.fr>
2024-06-06 00:44:02 +0200

    added_first

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhan <mhan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 23:14:36 by mhan              #+#    #+#             */
+/*   Updated: 2024/06/06 00:39:51 by mhan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SISZE 8
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
